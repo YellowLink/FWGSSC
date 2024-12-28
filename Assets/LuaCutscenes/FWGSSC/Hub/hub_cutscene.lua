37fd1638933fdfb870e16f2edc1ace5770cc8428
An-Enormous-Smiley-Face <anenormoussmileyface@gmail.com>
2024-12-28 17:12:38 -0500

    Update script for legibility, added remaining rooms

--- Assets/LuaCutscenes/FWGSSC/Hub/hub_cutscene.lua
+++ Assets/LuaCutscenes/FWGSSC/Hub/hub_cutscene.lua
@@ -1,19 +1,16 @@
 function onBegin()
     makeUnskippable()
-    if (arguments == "Hub_Intro") then
-        hubIntro()
-    end
-    while (not getFlag("tp_ready")) do
-        wait()
-    end
-    setFlag("tp_ready", false)
-    instantTeleport(arguments)
+    waitUntilBreaker()
+    hubTP()
 end
 
-function hubIntro()
+function waitUntilBreaker()
     while (not getFlag("disable_lightning")) do
         wait()
     end
     setFlag("disable_lightning", false)
-    teleportTo(1376,1688,"lobby1")
+end
+
+function hubTP()
+    instantTeleport("hub-main")
 end
