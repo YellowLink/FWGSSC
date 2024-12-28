9f6ba133b3562ad28b073f62a50e6e090c5b1ba5
An-Enormous-Smiley-Face <anenormoussmileyface@gmail.com>
2024-12-28 13:19:08 -0500

    Adjust scrip to take params, add two tp's

--- Assets/LuaCutscenes/FWGSSC/Hub/hub_cutscene.lua
+++ Assets/LuaCutscenes/FWGSSC/Hub/hub_cutscene.lua
@@ -1,4 +1,16 @@
 function onBegin()
+    makeUnskippable()
+    if (arguments == "Hub_Intro") then
+        hubIntro()
+    end
+    while (not getFlag("tp_ready")) do
+        wait()
+    end
+    setFlag("tp_ready", false)
+    instantTeleport(arguments)
+end
+
+function hubIntro()
     while (not getFlag("disable_lightning")) do
         wait()
     end
