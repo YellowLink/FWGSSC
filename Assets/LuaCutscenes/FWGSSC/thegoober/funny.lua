05950346f44ebd2438f07544ed62d1d0240102ce
An-Enormous-Smiley-Face <anenormoussmileyface@gmail.com>
2024-12-30 00:58:00 -0500

    Adds a funny to CRT

--- Assets/LuaCutscenes/FWGSSC/thegoober/funny.lua
+++ Assets/LuaCutscenes/FWGSSC/thegoober/funny.lua
@@ -1,3 +1,15 @@
 function onBegin()
-    say("hi")
+    makeUnskippable()
+    disableRetry()
+    start = os.time()
+    playSound("event:/char/dialogue/secret_character", player.Position)
+    say("STUCK")
+    while (os.time() - start < 17) do
+        wait(1)
+    end
+    die()
+end
+
+function onEnd()
+    enableRetry()
 end
